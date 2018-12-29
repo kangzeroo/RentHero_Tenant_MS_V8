da64217d2e44aad82bda04768fbf204fbe55546f
Jimmy Guo <jimmyzidanguo@hotmail.com>
2018-12-28 21:19:37 -0500

    asd

--- server.js
+++ server.js
@@ -63,11 +63,11 @@ if (process.env.NODE_ENV === 'production') {
       requestCert: false,
       rejectUnauthorized: false
   }
-  // const server = https.createServer(options, app)
-  const server = http.createServer(app)
+  const server = https.createServer(options, app)
+  // const server = http.createServer(app)
 
   // listen to the server on port
   server.listen(port, function(){
-    console.log("Server listening on http: ", port)
+    console.log("Server listening on https: ", port)
   })
 }
